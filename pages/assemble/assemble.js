1ca96f28f3b662bdede2d5de2e0bcd49cc3a99d6
杨家馨 <578755236@qq.com>
2019-05-09 19:14:03 +0800

    团购

--- pages/assemble/assemble.js
+++ pages/assemble/assemble.js
@@ -1,20 +1,37 @@
 // pages/assemble/assemble.js
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: ["自提拼团", "物流拼团"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
   },
-
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
