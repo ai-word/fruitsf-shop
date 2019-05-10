96366101a38285338a4cfff4262672e26c124e94
杨家馨 <578755236@qq.com>
2019-05-10 17:22:34 +0800

    分类页面

--- pages/classification/classification.js
+++ pages/classification/classification.js
@@ -45,48 +45,15 @@ Page({
         });
       }
     });
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-
-
-    wx.getSystemInfo({
-      success: (res) => {
-        this.setData({
-          pixelRatio: res.pixelRatio,
-          windowHeight: res.windowHeight,
-          windowWidth: res.windowWidth
-        })
-      },
-    })
   },
   switchNav(event) {
     this.setData({
       sliderOffset: event.currentTarget.offsetLeft,
       activeIndex: event.currentTarget.id
+    });
+    wx.pageScrollTo({
+      scrollTop: 300,
+      duration: 0
     });
     var cur = event.currentTarget.dataset.current;
     //每个tab选项宽度占1/5
