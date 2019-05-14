9cc830a00aa4ea0adf663e854b5294b4ce9a525c
杨家馨 <578755236@qq.com>
2019-05-14 17:47:27 +0800

    新增我的订单页

---
+++ pages/my-order/order.js
@@ -0,0 +1,78 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    navData: [
+      {
+        text: '全部'
+      },
+      {
+        text: '待付款'
+      },
+      {
+        text: '配送中'
+      },
+      {
+        text: '待自提'
+      },
+      {
+        text: '待评价'
+      }
+    ],
+    toView: 'the-0',
+    currentTab: 0,
+    navScrollLeft: 0,
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    windowHeight: ''
+  },
+  //事件处理函数
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.navData.length - sliderWidth) / 10,
+          sliderOffset: res.windowWidth / that.data.navData.length * that.data.activeIndex,
+          windowHeight: res.windowHeight
+        });
+      }
+    });
+  },
+  switchNav(event) {
+    console.log(event)
+    this.setData({
+      sliderOffset: event.currentTarget.offsetLeft,
+      toView: 'the-' + event.currentTarget.dataset.current
+    });
+    var cur = event.currentTarget.dataset.current;
+    //每个tab选项宽度占1/5
+    var singleNavWidth = this.data.windowWidth / 5;
+    //tab选项居中                            
+    this.setData({
+      navScrollLeft: (cur - 2) * singleNavWidth
+    })
+    if (this.data.currentTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+  switchTab(event) {
+    var cur = event.detail.current;
+    var singleNavWidth = this.data.windowWidth / 5;
+    this.setData({
+      currentTab: cur,
+      navScrollLeft: (cur - 2) * singleNavWidth
+    });
+  }
+})
