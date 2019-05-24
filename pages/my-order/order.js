7d049e133ac94a25b5b146919839b89c94d7d6a4
杨家馨 <578755236@qq.com>
2019-05-24 23:24:36 +0800

    我的订单

--- pages/my-order/order.js
+++ pages/my-order/order.js
@@ -1,11 +1,32 @@
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
-
+const Http = require('../../utils/request.js');
+const app = getApp();
 Page({
   data: {
-    tabs: ["全部", "待付款", "配送中", "待自提"],
+    tabs: [{
+      name: '全部',
+      status: 888,
+    }, {
+      name: '待付款',
+        status: 0,
+      }, {
+      name: '配送中',
+        status: 2,
+      }, {
+      name: '待自提',
+        status: 999,
+    }],
     activeIndex: 0,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    pageNumber: 1,
+    pageSize:10,
+    orderList: [],
+    status: '888',
+    orderId: '',
+    hasmoreData: false,
+    loaderMore: true,
+    hiddenloading: false,
   },
   onLoad: function () {
     var that = this;
@@ -17,11 +38,109 @@ Page({
         });
       }
     });
+    that.getOrderList()
   },
   tabClick: function (e) {
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id
+      activeIndex: e.currentTarget.id,
+      status: e.currentTarget.dataset.status
     });
-  }
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading'
+    })
+    this.data.orderList = []
+    this.getOrderList()
+  },
+  //获取全部订单列表
+  getOrderList() {
+    var that = this
+    let params = {
+      status: that.data.status,
+      pageNumber: this.data.pageNumber,
+      pageSize: this.data.pageSize
+    }
+    Http.HttpRequst(false, '/order/getOrders', true, '', params, 'get', false, function (res) {
+      if (res.state == 'ok') {
+        wx.hideToast()
+        if (res.data.list.length === 0 && that.data.pageNumber === 1) {
+          that.setData({
+            orderList: [],
+            hasmoreData: false
+          })
+          return false
+        }
+        if (res.data.list.length < that.data.pageSize) {
+          that.setData({
+            orderList: that.data.orderList.concat(res.data.list),
+          })
+          that.setData({
+            hasmoreData: true,
+            hiddenloading: false,
+            loaderMore: false
+          })
+        } else {
+          that.setData({
+            orderList: that.data.orderList.concat(res.data.list),
+          })
+        }
+      }
+    })
+  },
+  //取消订单
+  cancelOrder(e) {
+    var that = this
+    var orderId = e.currentTarget.dataset.orderid
+    wx.showModal({
+      title: '提示',
+      content: '确认要取消该订单？',
+      confirmText: "确认",
+      cancelText: "取消",
+      success: function (res) {
+        console.log(res);
+        if (res.confirm) {
+          console.log('用户点击主操作')
+          that.getCanelOrder(orderId)
+        } else {
+          console.log('用户点击辅助操作')
+        }
+      }
+    })
+  },
+  //取消订单
+  getCanelOrder(orderId) {
+    var that = this
+    let params = {
+      orderId: orderId
+    }
+    Http.HttpRequst(false, '/order/cancelOrder', true, '', params, 'post', false, function (res) {
+      if (res.state == 'ok') {
+        that.getOrderList()
+      }
+    })
+  },
+  //立即支付
+  immediatePay() {
+
+  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log('触底了');
+    var that = this
+    if (that.data.loaderMore) {
+      that.setData({
+        hasmoreData: false,
+        hiddenloading: true,
+      })
+      setTimeout(function () {
+        that.setData({
+          pageNumber: parseInt(that.data.pageNumber + 1)
+        })
+        that.getOrderList()
+      }, 500)
+    }
+  },
 });
